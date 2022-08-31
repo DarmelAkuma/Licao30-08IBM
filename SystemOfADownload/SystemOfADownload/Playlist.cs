faadd97c8275a7ff3a6996dd65ecb4c51e4ae889
Mateus Melo Oliveira <Mateus99Melo@gmail.com>
2022-08-31 10:35:05 -0300

    Commit inicial

---
+++ SystemOfADownload/SystemOfADownload/Playlist.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemOfADownload {
+    class Playlist {
+        private int Botao1 { get; }
+        private int Botao2 { get; }
+        private int Soma { get; set; }
+        public Playlist(int botao1, int botao2) {
+            Botao1 = botao1;
+            Botao2 = botao2;
+        }
+        public string SomaCodigo() {
+            Soma = Botao1 + Botao2;
+            switch (Soma) {
+                case 0:
+                    return "PROXYCITY";
+                case 1:
+                    return "P.Y.N.G.";
+                case 2:
+                    return "DNSUEY!";
+                case 3:
+                    return "SERVERS";
+                case 4:
+                    return "HOST!";
+                case 5:
+                    return "CRIPTONIZE";
+                case 6:
+                    return "OFFLINE DAY";
+                case 7:
+                    return "SALT";
+                case 8:
+                    return "ANSWER!";
+                case 9:
+                    return "RAR?";
+                case 10:
+                    return "WIFI ANTENNAS";
+                default:
+                    return "Sem musicas com esse código";
+            }
+        }
+    }
+}
---
+++ SystemOfADownload/SystemOfADownload/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SystemOfADownload {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite quantas músicas serão reproduzidas: ");
+            var C = int.Parse(Console.ReadLine());
+            for (int i = 0; i < C; i++) {
+                Console.WriteLine("Pressione dois botões de 0 à 5: ");
+                string[] valores = Console.ReadLine().Split(' ');
+                var botao1 = int.Parse(valores[0]);
+                var botao2 = int.Parse(valores[1]);
+                Playlist soma = new Playlist(botao1, botao2);
+                Console.WriteLine(soma.SomaCodigo());
+            }
+        }
+    }
+}
